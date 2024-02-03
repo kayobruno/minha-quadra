f1d42e0ba40977362d1ab3e71a8a931eafa4264a
Kayo Bruno <kayodw@gmail.com>
2024-02-03 20:12:47 -0300

    style: Apply PHP-CS-Fixer

--- app/Providers/MenuServiceProvider.php
+++ app/Providers/MenuServiceProvider.php
@@ -6,23 +6,23 @@ use Illuminate\Support\ServiceProvider;
 
 class MenuServiceProvider extends ServiceProvider
 {
-  /**
-   * Register services.
-   */
-  public function register(): void
-  {
-    //
-  }
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
 
-  /**
-   * Bootstrap services.
-   */
-  public function boot(): void
-  {
-    $verticalMenuJson = file_get_contents(base_path('resources/menu/verticalMenu.json'));
-    $verticalMenuData = json_decode($verticalMenuJson);
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $verticalMenuJson = file_get_contents(base_path('resources/menu/verticalMenu.json'));
+        $verticalMenuData = json_decode($verticalMenuJson);
 
-    // Share all menuData to all the views
-    \View::share('menuData', [$verticalMenuData]);
-  }
+        // Share all menuData to all the views
+        \View::share('menuData', [$verticalMenuData]);
+    }
 }
