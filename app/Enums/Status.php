728cb5bf979af2c42ffd82fae82ae9ebf2728a64
Kayo Bruno <kayodw@gmail.com>
2024-02-05 23:03:14 -0300

    feat: Add Status Enum

---
+++ app/Enums/Status.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+enum Status: string
+{
+    case Pending = 'pending';
+    case Active = 'active';
+    case Inactive = 'inactive';
+
+    public static function all(): array
+    {
+        $data = [];
+        foreach (self::cases() as $status) {
+            $data[] = $status->value;
+        }
+
+        return $data;
+    }
+
+    public function label(): string
+    {
+        return match ($this) {
+            Status::Pending => 'Pendente',
+            Status::Active => 'Ativo',
+            Status::Inactive => 'Inativo',
+        };
+    }
+}
