297590f43ecdc3bafd454bbc5e6d067265990e27
Kayo Bruno <kayodw@gmail.com>
2024-02-11 19:24:12 -0300

    feat: Add BladeServiceProvider to customize any functions to Blade

---
+++ app/Providers/BladeServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+
+class BladeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Blade::directive('money', function ($money) {
+            return "<?php echo number_format($money, 2, ',', '.'); ?>";
+        });
+    }
+}
