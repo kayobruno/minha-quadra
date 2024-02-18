f88c8800d554b3429cd9a987b3355da6e393e944
Kayo Bruno <kayodw@gmail.com>
2024-02-18 10:52:59 -0300

    chore: Update query to list Bookings

--- app/Http/Controllers/Dash/BookingController.php
+++ app/Http/Controllers/Dash/BookingController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Dash;
 
+use App\Enums\BookingStatus;
 use App\Http\Controllers\Controller;
 use App\Models\Booking;
 
@@ -11,7 +12,11 @@ class BookingController extends Controller
 {
     public function index()
     {
-        $bookings = Booking::with('customer', 'user', 'court')->paginate(10);
+        $bookings = Booking::with('customer', 'user', 'court')
+            ->whereDate('when', '>', now())
+            ->whereNotIn('status', [BookingStatus::Canceled, BookingStatus::Expired, BookingStatus::Finished])
+            ->orderBy('when', 'asc')
+            ->paginate(10);
 
         return view('content.bookings.index', compact('bookings'));
     }
