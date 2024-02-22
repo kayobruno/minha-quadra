6d4aa0c00d7d94a8369c396f9301d8c76efc8215
Kayo Bruno <kayodw@gmail.com>
2024-02-22 13:40:04 -0300

    feat: Add new states to Booking

---
+++ app/States/Booking/PendingBookingState.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\States\Booking;
+
+class PendingBookingState extends AbstractBookingState
+{
+    public function confirm(): void
+    {
+        // TODO:
+    }
+
+    public function cancel(): void
+    {
+        // TODO:
+    }
+
+    public function expire(): void
+    {
+        // TODO:
+    }
+}
---
+++ app/States/Booking/ProgressBookingState.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\States\Booking;
+
+class ProgressBookingState extends AbstractBookingState
+{
+    public function finish(): void
+    {
+        // TODO:
+    }
+}
