fd5782c0d9f899a7f920aca79059e57fbcc31382
Kayo Bruno <kayodw@gmail.com>
2024-03-17 16:38:27 -0300

    chore: Add relationship with merchant and new field color_rgba

--- app/Models/Court.php
+++ app/Models/Court.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Services\ColorConversionService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Court extends Model
 {
@@ -16,4 +18,16 @@ class Court extends Model
         'color',
         'merchant_id',
     ];
+
+    public function merchant(): BelongsTo
+    {
+        return $this->belongsTo(Merchant::class, 'id');
+    }
+
+    public function getColorRgbaAttribute(): string
+    {
+        $colorConversionService = new ColorConversionService();
+
+        return $colorConversionService->hexToRgba($this->color, 0.5);
+    }
 }
