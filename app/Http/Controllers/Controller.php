db3e098d07fcdff6a2f9619fca75fdb268beaefd
Kayo Bruno <kayodw@gmail.com>
2024-03-28 20:54:20 -0300

    feat: [API] Add method to build Unavailable Response default

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,11 +5,19 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Traits\Response;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests, Response;
+
+    protected function buildUnavailableResponse(): JsonResponse
+    {
+        $message = __('messages.errors.unavailable');
+        return $this->response(response: [], statusCode: SymfonyResponse::HTTP_SERVICE_UNAVAILABLE, message: $message, success: false);
+    }
 }
